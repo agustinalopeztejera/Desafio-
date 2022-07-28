87c635858bd8298fae78fde146688a66b3b2861e
Agustina Lopez Tejera <agustinalopeztejera@gmail.com>
2022-07-28 00:58:16 -0300

    viewdelmodelo

--- desafio/myfamily/models.py
+++ desafio/myfamily/models.py
@@ -1,9 +1,11 @@
+
 from django.db import models
 from django.forms import DateTimeField
+import datetime 
 
 class Family_members(models.Model):
     name = models.CharField(max_length=40)
     age = models.IntegerField()
     hobbies = models.CharField(max_length=60)
-    last_active_whapp = DateTimeField()
+    last_active_whapp = DateTimeField(datetime.now)
 
--- desafio/myfamily/views.py
+++ desafio/myfamily/views.py
@@ -1,3 +1,10 @@
+from xmlrpc.client import DateTime
 from django.shortcuts import render
+from myfamily.models import Family_members
 
-# Create your views here.
+def create_family_member(request):
+    new_member = Family_members.objects.create(name = 'John Smith', 
+    age = 40, 
+    hobbies = 'Playing football', 
+    last_active_whapp = DateTime.now())
+
