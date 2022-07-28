7d29710ed3dbf7e19cacb92fb2773bb5c48ea8ae
Agustina Lopez Tejera <agustinalopeztejera@gmail.com>
2022-07-28 00:32:43 -0300

    primeraapp

---
+++ desafio/myfamily/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.forms import DateTimeField
+
+class Family_members(models.Model):
+    name = models.CharField(max_length=40)
+    age = models.IntegerField()
+    hobbies = models.CharField(max_length=60)
+    last_active_whapp = DateTimeField()
+
