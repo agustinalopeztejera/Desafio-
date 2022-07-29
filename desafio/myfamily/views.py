7141f92109ff9d20111c02370b7cbe8ebc5680f9
Agustina Lopez Tejera <agustinalopeztejera@gmail.com>
2022-07-28 22:54:20 -0300

    secondtry

--- desafio/myfamily/views.py
+++ desafio/myfamily/views.py
@@ -4,10 +4,10 @@ from datetime import datetime
 from myfamily.models import Family_members
 
 def my_dad(request):
-    today = datetime.now().date
-    family_member = Family_members.objects.create(name = 'John Smith', 
+    today = datetime.now()
+    my_dad = Family_members.objects.create(name = 'John Smith', 
     age = '50', 
     hobbies = 'playing football',
-    last_active_whapp = (today))
+    last_active_whapp = today)
     context = {context}
-    return render(request, 'family_member', context=context)
+    return render(request, 'template_my_dad.html', context=context)
