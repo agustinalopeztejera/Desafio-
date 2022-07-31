bd4154cd68154ffa4f0a35a41e4e667939ee2c8a
Agustina Lopez Tejera <agustinalopeztejera@gmail.com>
2022-07-31 01:09:04 -0300

    version_final

--- desafio/myfamily/models.py
+++ desafio/myfamily/models.py
@@ -4,3 +4,4 @@ class Family_members(models.Model):
     name = models.CharField(max_length=40)
     age = models.IntegerField()
     hobbies = models.CharField(max_length=60)
+    birthday = models.DateField()
--- desafio/myfamily/views.py
+++ desafio/myfamily/views.py
@@ -1,5 +1,4 @@
 from multiprocessing import context
-from xmlrpc.client import NOT_WELLFORMED_ERROR
 from django.shortcuts import render
 from datetime import datetime
 from myfamily.models import Family_members
@@ -7,29 +6,29 @@ from myfamily.models import Family_members
 
 def my_dad(request):
    
-    f = Family_members(name = 'John Smith',  age = 50,  hobbies = 'playing football')
+    f = Family_members(name = 'John Smith',  age = 50,  hobbies = 'jugar al futbol', birthday = '1972-02-19')
     f.save()
     
-    context = {"edad":"50", "hobbies":'playing football'}
+    context = {'edad':'50', 'hobbies':'jugar al futbol', 'birthday':'1972-02-19'}
     return render(request, 'template_my_dad.html', context=context)
 
 
 def my_mom(request):
    
-    f = Family_members(name = 'Anna Smith',  age = 40,  hobbies = 'knitting')
+    f = Family_members(name = 'Anna Smith',  age = 40,  hobbies = 'pintar', birthday = '1982-01-13')
     f.save()
     
     
-    context = {"edad":"50", "hobbies":'knitting', }
+    context = {'edad':'40', 'hobbies':'pintar', 'birthday': '1982-01-13'}
     return render(request, 'template_my_mom.html', context=context)
 
 def my_bro(request):
    
-    f = Family_members(name = 'Luke Smith',  age = 22,  hobbies = 'playing the guitar')
+    f = Family_members(name = 'Luke Smith',  age = 21,  hobbies = 'tocar la guitarra', birthday = '2000-10-23')
     f.save()
     
     
-    context = {"edad":"50", "hobbies":'playing the guitar', }
+    context = {'edad':'21', 'hobbies':'tocar la guitarra', 'birthday': '2000-10-23'}
     return render(request, 'template_my_bro.html', context=context)
 
 
